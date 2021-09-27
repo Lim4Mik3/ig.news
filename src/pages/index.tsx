f461c13768b6b86cf7e0b2cf1239fcd96731f754
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-09-27 00:26:34 -0300

    Global styled created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import homeStyles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>ig.news | Início</title>
       </Head>
-      <h1 className={homeStyles.title}>Hello <span>World!</span></h1>
+      <h1 >Hello world</h1>
     </>
   )
 }
